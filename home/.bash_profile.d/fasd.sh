6a018b5ace3c700ad424c4c894338d58770913e2
Dominique Broeglin <dominique.broeglin@gmail.com>
2012-12-25 20:16:15 +0100

    Adding fasd support

---
+++ home/.bash_profile.d/fasd.sh
@@ -0,0 +1,17 @@
+# Shameless ripoff of https://github.com/skwp/dotfiles/blob/master/zsh/fasd.zsh
+# only init if installed.
+fasd_cache="$HOME/.fasd-init-bash"
+if [ "$(command -v fasd)" -nt "$fasd_cache" -o ! -s "$fasd_cache" ]; then
+	  eval "$(fasd --init posix-alias bash-hook bash-ccomp bash-ccomp-install)" >| "$fasd_cache"
+fi
+source "$fasd_cache"
+unset fasd_cache
+
+
+# jump to recently used items
+alias a='fasd -a' # any
+alias s='fasd -si' # show / search / select
+alias d='fasd -d' # directory
+alias f='fasd -f' # file
+alias z='fasd_cd -d' # cd, same functionality as j in autojump
+alias zz='fasd_cd -d -i' # interactive directory jump
