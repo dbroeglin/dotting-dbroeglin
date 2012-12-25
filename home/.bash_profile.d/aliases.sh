ff0f6adc24ab85998210ba45ccc3b02a6128128a
Dominique Broeglin <dominique.broeglin@gmail.com>
2012-12-25 21:44:21 +0100

    Added aliases for maven

--- home/.bash_profile.d/aliases.sh
+++ home/.bash_profile.d/aliases.sh
@@ -85,3 +85,15 @@ alias cl='clear'
 # Hi, Leon, cleaner 
 alias ka9='killall -9'
 alias k9='kill -9'
+
+# Maven aliases
+alias mci='mvn clean install'
+alias mi='mvn install'
+alias mcp='mvn clean package'
+alias mp='mvn package'
+alias mrprep='mvn release:prepare'
+alias mrperf='mvn release:perform'
+alias mrrb='mvn release:rollback'
+alias mdep='mvn dependency:tree'
+alias mpom='mvn help:effective-pom'
+alias mcisk='mci -Dmaven.test.skip=true'
