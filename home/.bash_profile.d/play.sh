2cf9bec57aa5e98a393f2484589d34d45fdcf15f
Dominique Broeglin <dominique.broeglin@gmail.com>
2013-09-07 00:10:36 +0200

    Mobing to play 2.1.1

--- home/.bash_profile.d/play.sh
+++ home/.bash_profile.d/play.sh
@@ -1,12 +1,15 @@
 function play-env {
 	case $1 in
+	1.2*)
+		export PLAY_HOME="$HOME/Applications/play-$1"
+  ;;
 	1*)
 		export PLAY_HOME="$HOME/Applications/play-1.2.5"
 	;;
 	2*)
-		export PLAY_HOME="$HOME/Applications/play-2.0.4"
+		export PLAY_HOME="$HOME/Applications/play-2.1.1"
 	;;
-	*)
+  *)
 		echo "Usage $0 (1|2)"
 	;;
 	esac
